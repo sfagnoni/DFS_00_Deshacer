f49842561983731c45d93aa13f4c7627e5338651
Santiago <sfagnoni@gmail.com>
2025-08-11 22:28:48 -0300

    docs: Agrega instrucciones para guiar la aplicación

--- app.js
+++ app.js
@@ -18,21 +18,22 @@ function crearBolilla(numero) {
 function eliminarBolilla(evento) {
     const bolilla = evento.currentTarget;
     bolilla.classList.add('removing');
-    bolillas = bolillas.filter(b => b !== bolilla);
+    bolillas = bolillas.filter(b => b !== bolilla);//ver histórico de bolillas
     setTimeout(() => bolilla.remove(), 180);
 }
 
 function sortearBolilla() {
     const numero = obtenerBolillaAleatoria();
     const bolilla = crearBolilla(numero);
-    bolillas.push(bolilla);
+    bolillas.push(bolilla);//ver histórico de bolillas
     contenedorBolillas.appendChild(bolilla);
 }
 
 botonSortear.addEventListener('click', sortearBolilla);
 
 window.addEventListener("keydown", (evento) => {
-    
+    //a continuación el ctrl + z elimina el último número agregado
+    //la idea es modificar esto para que se deshaga la última acción
     if (evento.key === "z" && evento.ctrlKey) {
         bolillas[bolillas.length - 1].classList.add('removing');
         setTimeout(() => {
